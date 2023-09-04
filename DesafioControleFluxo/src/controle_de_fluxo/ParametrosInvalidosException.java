c3df757cbd350600af7cd702bb2b6f366d26378d
ALEX WILSON GUEDES DA SILVA <alex__guedes@hotmail.com>
2023-09-04 09:29:17 -0300

    Add files via upload

---
+++ DesafioControleFluxo/src/controle_de_fluxo/ParametrosInvalidosException.java
@@ -0,0 +1,13 @@
+package controle_de_fluxo;
+
+public class ParametrosInvalidosException  extends Exception{
+
+	public ParametrosInvalidosException() {
+        super();
+    }
+	
+	 public ParametrosInvalidosException(String mensagem) {
+	        super(mensagem);
+	    }
+
+}
